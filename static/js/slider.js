eb10228d2836061458ecb7f6901a862a7a690446
AndeyGordeev <prolodgy@yandex.ru>
2017-11-28 23:45:24 +0300

    Slaider is ready

--- static/js/slider.js
+++ static/js/slider.js
@@ -2,6 +2,7 @@ var slideNow = 1;
 var slideCount = $('#slidewrapper').children().length;
 var translateWidth = 0;
 var slideInterval = 2000;
+var navBtnId = 0;
 
 function nextSlide() {
     if (slideNow === slideCount || slideNow <= 0 || slideNow > slideCount) {
@@ -18,6 +19,56 @@ function nextSlide() {
     }
 }
 
+function prevSlide() {
+    if (slideNow === 1 || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('#viewport').width() * (slideCount - 1);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow - 2);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow--;
+    }
+}
+
 $(document).ready(function () {
-    setInterval(nextSlide, slideInterval);
+    var switchInterval = setInterval(nextSlide, slideInterval);
+
+    $('#viewport').hover(function(){
+        clearInterval(switchInterval);
+    },function() {
+        switchInterval = setInterval(nextSlide, slideInterval);
+    });
+
+    $('#next-btn').click(function() {
+        nextSlide();
+    });
+
+    $('#prev-btn').click(function() {
+        prevSlide();
+    });
+
+    $('.slide-nav-btn').click(function() {
+        navBtnId = $(this).index();
+
+        if (navBtnId + 1 !== slideNow) {
+            $('.slide-nav-btn').css('background-color', '#fff');
+            translateWidth = -$('#viewport').width() * (navBtnId);
+            $('#slidewrapper').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)'
+            });
+            $(this).css('background-color', '#000');
+            slideNow = navBtnId + 1;
+        }
+    });
 });
