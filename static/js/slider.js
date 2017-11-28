870b8a22c57581a6bf119f2619c9964dfc7cefb9
AndeyGordeev <prolodgy@yandex.ru>
2017-11-29 00:35:18 +0300

    Slider is full ready with size and full vision

--- static/js/slider.js
+++ static/js/slider.js
@@ -5,9 +5,11 @@ var slideInterval = 2000;
 var navBtnId = 0;
 
 function nextSlide() {
+    $('.slide-nav-btn').eq(slideNow-1).css('background-color', '#fff');
     if (slideNow === slideCount || slideNow <= 0 || slideNow > slideCount) {
         $('#slidewrapper').css('transform', 'translate(0, 0)');
         slideNow = 1;
+        $('.slide-nav-btn').eq(slideNow-1).css('background-color', '#000');
     } else {
         translateWidth = -$('#viewport').width() * (slideNow);
         $('#slidewrapper').css({
@@ -16,31 +18,37 @@ function nextSlide() {
             '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
         });
         slideNow++;
+        $('.slide-nav-btn').eq(slideNow-1).css('background-color', '#000');
     }
 }
 
 function prevSlide() {
+    $('.slide-nav-btn').eq(slideNow-1).css('background-color', '#fff');
     if (slideNow === 1 || slideNow <= 0 || slideNow > slideCount) {
         translateWidth = -$('#viewport').width() * (slideCount - 1);
         $('#slidewrapper').css({
             'transform': 'translate(' + translateWidth + 'px, 0)',
             '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)'
         });
         slideNow = slideCount;
+        $('.slide-nav-btn').eq(slideNow-1).css('background-color', '#000');
     } else {
         translateWidth = -$('#viewport').width() * (slideNow - 2);
         $('#slidewrapper').css({
             'transform': 'translate(' + translateWidth + 'px, 0)',
             '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)'
         });
         slideNow--;
+        $('.slide-nav-btn').eq(slideNow-1).css('background-color', '#000');
     }
 }
 
 $(document).ready(function () {
     var switchInterval = setInterval(nextSlide, slideInterval);
+
+    $('.slide-nav-btn').eq(slideNow-1).css('background-color', '#000');
 
     $('#viewport').hover(function(){
         clearInterval(switchInterval);
