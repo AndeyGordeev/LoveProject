7a4249be6eaf84a541bc815c038336ce949d5476
AndeyGordeev <prolodgy@yandex.ru>
2017-11-29 03:58:44 +0300

    Create love message service

---
+++ loveMessage/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from loveMessage.models import LoveMessage
+
+admin.site.register(LoveMessage)
---
+++ loveMessage/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+
+class LoveMessage(models.Model):
+    user = models.ForeignKey(User, related_name='loveMessage')
+    recorded_at = models.DateTimeField(default=timezone.now, editable=False)
+    topic = models.CharField(max_length=128)
+    note = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return '{} : {}'.format(self.recorded_at.strftime('%Y-%m-%d %H:%M:%S'), self.topic)
